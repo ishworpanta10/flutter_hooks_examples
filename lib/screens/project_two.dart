5182081aca189943c9b29475bbb60fdf7806cb6c
ishworpanta10 <ishworpanta10@gmail.com>
2022-07-30 18:55:07 +0545

    completed project two with useState,useTextEditingControler and useEffect

--- lib/screens/project_two.dart
+++ lib/screens/project_two.dart
@@ -6,12 +6,37 @@ class ProjectTwo extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = useTextEditingController();
+    final text = useState('');
+
+    useEffect(() {
+      controller.addListener(() {
+        text.value = controller.text;
+      });
+      return null;
+    }, [controller]);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Hook Demo'),
       ),
-      body: const Center(
-        child: Text('Project Two'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'You Type : ${text.value}',
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: controller,
+              decoration: const InputDecoration(hintText: 'Please type here .....'),
+            ),
+          ],
+        ),
       ),
     );
   }
